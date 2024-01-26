1a025c5b1551ad915e57c9b87151a9992359cd62
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2024-01-26 22:38:44 +0000

    Deploying to main from @ Sergeyy321/Rent-cars@3017b2cf1ad42e471b437a85be03ed5f79640e98 🚀

--- src/components/Loader/Loader.styled.jsx
+++
@@ -1,9 +0,0 @@
-import { styled } from "styled-components";
-
-export const LoaderStyled = styled.div`
-  justify-content: center;
-  align-items: center;
-  margin: auto;
-
-  max-width: 100px;
-`;
