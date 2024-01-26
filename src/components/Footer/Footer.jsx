ee37a94065c019cecb9557d84044fb224cb4209b
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2024-01-26 22:34:35 +0000

    Deploying to gh-pages from @ Sergeyy321/Rent-cars@494ba599824a71cbd4159baa95d7524280e0a80d 🚀

--- src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import { FooterStyled, Copyright } from "./Footer.styled";
-export const Footer = () => {
-  return (
-    <FooterStyled>
-      <Copyright>All right reserved &copy;</Copyright>
-    </FooterStyled>
-  );
-}
